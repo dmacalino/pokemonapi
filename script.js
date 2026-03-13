8347855a0bc852f088e6a18d0d794c52b3073d2a
dmacalino <dmacalino@elisava.net>
2026-03-13 17:50:48 +0100

    Implement renderPokemon function to display Pokémon

--- script.js
+++ script.js
@@ -24,3 +24,32 @@ const pokemonData = [
     image: "img/squirtle.png"
   }
 ];
+
+const container = document.getElementById("pokemon-container");
+
+function renderPokemon(pokemonList) {
+  container.innerHTML = "";
+
+  pokemonList.forEach((pokemon) => {
+    const typeBadges = pokemon.type
+      .map(
+        (type) => `<span class="type-badge ${type}">${type}</span>`
+      )
+      .join("");
+
+    const card = `
+      <div class="card">
+        <img src="${pokemon.image}" alt="${pokemon.name}">
+        <p class="number">${pokemon.id}</p>
+        <h2 class="name">${pokemon.name}</h2>
+        <div class="types">${typeBadges}</div>
+        <div class="stats">
+          <span>${pokemon.height}</span>
+          <span>${pokemon.weight}</span>
+        </div>
+      </div>
+    `;
+
+    container.innerHTML += card;
+  });
+}
