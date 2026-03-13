6e2182c7c9c0d0bb530e6d6d62548a20304a223f
dmacalino <dmacalino@elisava.net>
2026-03-13 17:50:24 +0100

    Update pokemon data structure with detailed information
    Replaced the existing pokemon array with a more detailed pokemonData array that includes id, height, weight, and image properties.

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-const pokemon = [
-
-{ name:"Charmander", type:"fire" },
-{ name:"Squirtle", type:"water" },
-{ name:"Pidgey", type:"normal" }
-
-]
-
-function showType(type){
-
-const container = document.getElementById("pokemon")
-
-container.innerHTML = ""
-
-pokemon.forEach(p => {
-
-if(type === "all" || p.type === type){
-
-container.innerHTML += `
-<div class="card">
-<h2>${p.name}</h2>
-<p>${p.type}</p>
-</div>
-`
-
-}
-
-})
-
-}
-
-showType("all")
+const pokemonData = [
+  {
+    id: "#016",
+    name: "Pidgey",
+    type: ["normal"],
+    height: "3M",
+    weight: "18KG",
+    image: "img/pidgey.png"
+  },
+  {
+    id: "#004",
+    name: "Charmander",
+    type: ["fire"],
+    height: "6M",
+    weight: "85KG",
+    image: "img/charmander.png"
+  },
+  {
+    id: "#007",
+    name: "Squirtle",
+    type: ["water"],
+    height: "5M",
+    weight: "90KG",
+    image: "img/squirtle.png"
+  }
+];
